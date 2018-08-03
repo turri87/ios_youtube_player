486f42bb2592aa3851348138050e18199bc76134
adrianprieto <adrianprieto@dhcp-100-120-52-196.lon.corp.google.com>
2018-08-03 12:30:58 +0100

    Initial Commit

---
+++ TestPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestPlayer
+//
+//  Created by Adrian Prieto on 8/3/18.
+//  Copyright © 2018 Adrian Prieto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
